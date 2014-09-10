d08fa433d86832b996f7e93f889f7029aa1df185
Steve Erhart <erhart@email.unc.edu>
2014-09-10 15:32:15 -0400

    moved position added formatting for some columns

--- src/Hoo/Admin/LocationList.php
+++ src/Hoo/Admin/LocationList.php
@@ -17,8 +17,8 @@ class LocationList extends \WP_List_Table {
   public function get_columns() {
     return array(
       'name' => __( 'Name' ),
-      'position' => __( 'Position' ),
       'updated_at' => __( 'Modified Date' ),
+      'position' => __( 'Position' ),
       'is_visible' => __( 'Visible?' )
     );
   }
@@ -28,7 +28,7 @@ class LocationList extends \WP_List_Table {
 
 
     /**
-       TODO: pagination
+       TODO: pagination?
      */
 
 
@@ -50,6 +50,15 @@ class LocationList extends \WP_List_Table {
     );
 
     return sprintf( '%1$s %2$s', $location->name, $this->row_actions( $actions ) );
+  }
+  
+  public function column_updated_at( $location ) {
+    return $location->updated_at->format( 'F j, Y g:i a' );
+  }
+  
+  public function column_is_visible( $location ) {
+    $checked = $location->is_visible ? 'checked' : '';
+    return sprintf( '<input type="checkbox" value="%s" %s/>', $location->is_visible, $checked );
   }
 
   public function column_default( $location, $column_name ) {
