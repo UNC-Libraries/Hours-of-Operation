e7fad585db3b0dba4e7446bfa721447a22dc07ed
Steve Erhart <erhart@email.unc.edu>
2014-09-15 16:04:57 -0400

    added sortable columns

--- src/Hoo/Admin/LocationList.php
+++ src/Hoo/Admin/LocationList.php
@@ -23,22 +23,31 @@ class LocationList extends \WP_List_Table {
     );
   }
 
+  public function get_sortable_columns() {
+    return array(
+      'name' => array( 'name', false ),
+      'updated_at' => array( 'updated_at', false ),
+      'position' => array( 'position', true )
+    );
+  }
+
   public function prepare_items() {
     $current_screen = get_current_screen();
 
-
-    /**
-       TODO: pagination?
-     */
-
-
+    
     // register columns
     $columns = $this->get_columns();
-    $this->_column_headers = array( $columns, array(), array() );
+    $this->_column_headers = array(
+      $this->get_columns(),
+      array(),
+      $this->get_sortable_columns()
+    );
 
     // fetch locations
+    
+    $order_by = isset( $_REQUEST['orderby'] ) ? array( $_REQUEST['orderby'] => $_REQUEST['order'] ) : array( 'position' => 'asc' );
     $locations_repo = $this->entity_manager->getRepository( '\Hoo\Model\Location' );
-    $locations = $locations_repo->findAll();
+    $locations = $locations_repo->findBy( array(), $order_by );
 
     $this->items = $locations;
   }
@@ -51,11 +60,11 @@ class LocationList extends \WP_List_Table {
 
     return sprintf( '%1$s %2$s', $location->name, $this->row_actions( $actions ) );
   }
-  
+
   public function column_updated_at( $location ) {
     return $location->updated_at->format( 'F j, Y g:i a' );
   }
-  
+
   public function column_is_visible( $location ) {
     $checked = $location->is_visible ? 'checked' : '';
     return sprintf( '<input type="checkbox" value="%s" %s/>', $location->is_visible, $checked );
