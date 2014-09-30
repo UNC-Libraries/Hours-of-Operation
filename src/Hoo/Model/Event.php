cef16618b60d0dbad6bc29e9cbf722d48e4e77d8
Steve Erhart <erhart@email.unc.edu>
2014-09-30 15:15:26 -0400

    added event model

---
+++ src/Hoo/Model/Event.php
@@ -0,0 +1,81 @@
+<?php
+namespace Hoo\Model;
+
+/**
+   @ORM\Entity
+   @ORM\Table(name="hoo_events")
+   @ORM\HasLifecycleCallbacks()
+ */
+class Event {
+  /**
+     @ORM\Id
+     @ORM\Column(type="integer")
+     @ORM\GeneratedValue
+   */
+  private $id;
+
+  /** @ORM\Column(type="string", length=256) */
+  protected $label;
+
+  /** @ORM\Column(type="datetime") */
+  protected $start
+
+  /** @ORM\Column(type="datetime") */
+  protected $end
+
+  /** @ORM\OneToOne(targetEntity="Category") */
+  protected $category
+
+  /** @ORM\Column(type="boolean", options={"default"=0}) */
+  protected $is_all_day = false;
+
+  /** @ORM\Column(type="boolean", options={"default=1"}) */
+  protected $is_visible = true;
+
+  /** @ORM\Column(type="text", nullable=true) */
+  protected $recurrence_rule;
+
+  /** @ORM\Column(name="created_at", type="datetime") */
+  private $created_at;
+
+  /** @ORM\column(name="updated_at", type="datetime") */
+  private $updated_at;
+
+  /** @ORM\PrePersist */
+  public function set_created_at() {
+    $datetime = new \DateTime();
+    $this->updated_at = $datetime;
+    $this->created_at = $datetime;
+  }
+
+  /** @ORM\PreUpdate */
+  public function set_updated_at() {
+    $this->updated_at = new \DateTime();
+  }
+
+  public function __toString(){
+    return $this->label;
+  }
+
+  /**
+     a little getter/setter magic
+   */
+  public function __get( $property ) {
+
+    if ( property_exists( $this, $property ) ){
+      return $this->$property;
+    }
+  }
+
+  public function __set( $property, $value ) {
+
+    if ( property_exists( $this, $property ) ) {
+      $this->$property = $value;
+    } else {
+      trigger_error( "Can't access property " . get_class( $this ) . ':' . $property, E_USER_ERROR );
+    }
+
+    return $this; // allow chaining
+  }
+}
+?>
