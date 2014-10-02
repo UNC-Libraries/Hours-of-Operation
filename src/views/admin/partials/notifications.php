96d75cf3d66da734abb03d7aaa689e400de04c42
Steve Erhart <erhart@email.unc.edu>
2014-10-02 11:14:26 -0400

    only show div if we have a notification

--- src/views/admin/partials/notifications.php
+++ src/views/admin/partials/notifications.php
@@ -1,3 +1,5 @@
-<div class="below-h2 <?php echo $this['notification']['type'] ?>">
-  <p><?php _e( $this['notification']['message'], 'hoo-location' ) ?></p>
-</div>
+<?php if ( isset( $this['notification'] ) ) :?>
+  <div class="below-h2 <?php echo $this['notification']['type'] ?>">
+    <p><?php _e( $this['notification']['message'], 'hoo-location' ) ?></p>
+  </div>
+<?php endif ?>
