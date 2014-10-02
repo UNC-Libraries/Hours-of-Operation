3460c6a0d4608953f0bba2e1674fdce1362be417
Steve Segedy <segedy@email.unc.edu>
2014-10-02 14:30:28 -0400

    modified publish fields to match that for locations, removing 'priority' field

--- src/views/admin/partials/category_form_publish_fields.php
+++ src/views/admin/partials/category_form_publish_fields.php
@@ -4,18 +4,6 @@
       <li>
         <div class="js-wpt-field wpt-field js-wpt-textfield wpt-textfield">
           <div class="form-item form-item-textfield">
-            <label for="category_position">Priority</label>
-            <input name="category[priority]"
-                   type="text"
-                   size="4"
-                   id="category_priority"
-                   value="<?php echo $this['category']->priority ?>">
-          </div>
-        </div>
-      </li>
-      <li>
-        <div class="js-wpt-field wpt-field js-wpt-textfield wpt-textfield">
-          <div class="form-item form-item-textfield">
             <label for="category_is_visible">Visible</label>
             <input type="hidden" name="category[is_visible]" value="0"/>
             <input type="checkbox"
