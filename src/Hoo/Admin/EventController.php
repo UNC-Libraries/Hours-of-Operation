4ffbe8afae0f7a3e49929b795660f103daee5d90
Steve Erhart <erhart@email.unc.edu>
2014-10-03 16:01:55 -0400

    creating event working now

--- src/Hoo/Admin/EventController.php
+++ src/Hoo/Admin/EventController.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace Hoo\Admin;
 
@@ -29,29 +29,29 @@ class EventController {
       'slug' => 'hoo-location-event-edit'
     )
   );
-  
+
   public function __construct( $entity_manager ) {
     $this->entity_manager = $entity_manager;
-   
+
     $this->init_hooks();
   }
-  
+
   public function init_hooks() {
     add_action( 'admin_menu', array( $this, 'add_menu_pages' ) );
     add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
   }
-  
+
   public function enqueue_scripts() {
     $current_screen = get_current_screen();
 
-    // only enqueue for location pages
-    if ( preg_match( '/hoo-location(-event)?/i', $current_screen->id ) ) {
+    // enqueue edit/add page specific js
+    if ( preg_match( '/hoo-location-event-(add|edit)?/i', $current_screen->id ) ) {
       wp_enqueue_script( 'event-edit' );
-      wp_enqueue_style( 'jquery-ui' ); 
-      wp_enqueue_style( 'full-calendar' ); 
+      wp_enqueue_style( 'jquery-ui' );
+      wp_enqueue_style( 'full-calendar' );
     }
   }
-  
+
   public function add_menu_pages() {
     foreach ( $this->sub_pages as $sub_page => $options ) {
       add_submenu_page(
@@ -63,12 +63,12 @@ class EventController {
         array( $this, $sub_page ) );
     }
   }
-  
+
   public function add_meta_boxes( $page, $event ) {
     $event_details_fields = new View( 'admin/event/form_details_fields' );
     $event_publish_fields = new View( 'admin/event/form_publish_fields' );
     $event_general_fields = new View( 'admin/event/form_general_fields' );
-    
+
     add_meta_box( 'event-publish',
                   'Publish',
                   array( $event_publish_fields, 'render_metabox' ),
@@ -97,10 +97,11 @@ class EventController {
                   array( 'event' => $event ) );
 
   }
-  
+
   public function index() {
     $location = $this->entity_manager->find( '\Hoo\Model\Location', $_GET['location_id'] );
 
+    $view = new View( 'admin/event/index' );
     $view_options = array( 'title' => sprintf( '%s Hours Events', $location->name ) );
 
     $events_table = new EventList( $this->entity_manager, $location );
@@ -108,29 +109,43 @@ class EventController {
     $events_table->prepare_items();
     $view_options['events-table'] = $events_table;
 
-    $view = new View( 'admin/event/index' );
     $view->render( $view_options );
   }
-  
-  public function add() {
-    $location = $this->entity_manager->find( '\Hoo\Model\Location', $_GET['location_id'] );
 
-    $view = new View( 'admin/event/event' );
-    $view_options = array( 'page' => 'hoo-location-event-add',
-                           'columns' => 2 );
+  public function add() {
 
     if ( $_POST['action'] == 'create' ) {
-      //stub
+      $event_data = $_POST['event'];
+
+      $event_data['location'] = $this->entity_manager->find( '\Hoo\Model\Location', $event_data['location'] );
+      $event_data['category'] = $this->entity_manager->find( '\Hoo\Model\Category', $event_data['category'] );
+      $event_data['start'] = new \Datetime( $event_data['start'] );
+      $event_data['end'] = new \Datetime( $event_data['end'] );
+
+      $event = new Event( $event_data );
+      $this->entity_manager->persist( $event );
+      $this->entity_manager->flush();
+
+      wp_safe_redirect( admin_url( sprintf( 'admin.php?page=%s&location_id=%s', 'hoo-location-events', $even_data['location']->id ) ) );
+      exit;
     } else {
+      $location = $this->entity_manager->find( '\Hoo\Model\Location', $_GET['location_id'] );
       $event = new Event();
+
+      $view_options = array( 'page' => 'hoo-location-event-add',
+                             'columns' => 2 );
 
       $this->add_meta_boxes( $view_options['page'], $event );
       $view_options = array_merge( $view_options, array( 'title' => sprintf( 'Add an Hours Event for <em>%s</em>', $location->name ),
                                                          'event' => $event,
+                                                         'location' => $location,
                                                          'action' => 'create',
                                                          'action-display' => 'Add' ) );
+
+      $view = new View( 'admin/event/event' );
+      $view->render( $view_options );
     }
-    $view->render( $view_options );
+
   }
 }
- ?>
+?>
--- src/Hoo/Admin/EventList.php
+++ src/Hoo/Admin/EventList.php
@@ -2,6 +2,8 @@
 
 namespace Hoo\Admin;
 
+use Hoo\Utils;
+
 class EventList extends \WP_List_Table {
   public function __construct( $entity_manager, $location ) {
     $this->entity_manager = $entity_manager;
@@ -14,9 +16,9 @@ class EventList extends \WP_List_Table {
 
   public function get_columns() {
     return array( 'label'    => __( 'Title / Label' ),
+                  'category' => __( 'Category' ),
                   'start'    => __( 'Start' ),
-                  'end'      => __( 'End' ),
-                  'category' => __( 'Category' ));
+                  'end'      => __( 'End' ) );
   }
 
   public function get_sortable_columns() {
@@ -34,9 +36,34 @@ class EventList extends \WP_List_Table {
     // fetch events
     $order_by = isset( $_GET['orderby'] ) ? array( $_GET['orderby'] => $_GET['order'] ) : array( 'start' => 'desc' );
     $events_repo = $this->entity_manager->getRepository( '\Hoo\Model\Event' );
-    $events = $events_repo->findBy( array( 'location' => $location->id ), $order_by ) ;
+    $events = $events_repo->findBy( array( 'location' => $this->location->id ), $order_by );
 
     $this->items = $events;
+  }
+
+  public function column_label( $event ) {
+    $actions = array(
+      'edit' => sprintf( '<a href="?page=%s&location_id=%s">Edit</a>', 'hoo-location-event-edit', $event->location->id ),
+      'delete' => sprintf( '<a href="?page=%s&action=%s&location_id=%s" class="event-delete">Delete</a>', 'hoo-location-event-edit', 'delete', $event->location->id )
+    );
+
+    return sprintf( '%1$s %2$s', $event->label, $this->row_actions( $actions ) );
+  }
+  
+  public function column_category( $event ) {
+    return $event->category->name;
+  }
+  
+  public function column_start( $event ) {
+    return $event->start->format( 'Y-m-d h:i');
+  }
+
+  public function column_end( $event ) {
+    return $event->end->format( 'Y-m-d h:i');
+  }
+  
+  public function column_default( $event, $column_name ) {
+    return $event->$column_name;
   }
   
   public function no_items() {
