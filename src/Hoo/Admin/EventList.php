b62e220eba89f07b05cd0d9d68538067d46e438d
Steve Erhart <erhart@email.unc.edu>
2014-10-06 11:15:03 -0400

    location_id not needed in link

--- src/Hoo/Admin/EventList.php
+++ src/Hoo/Admin/EventList.php
@@ -43,8 +43,8 @@ class EventList extends \WP_List_Table {
 
   public function column_label( $event ) {
     $actions = array(
-      'edit' => sprintf( '<a href="?page=%s&location_id=%s&event_id=%s">Edit</a>', 'hoo-location-event-edit', $event->location->id, $event->id ),
-      'delete' => sprintf( '<a href="?page=%s&action=%s&location_id=%s" class="event-delete">Delete</a>', 'hoo-location-event-edit', 'delete', $event->location->id )
+      'edit' => sprintf( '<a href="?page=%s&event_id=%s">Edit</a>', 'hoo-location-event-edit', $event->id ),
+      'delete' => sprintf( '<a href="?page=%s&action=%s&event_id=%s" class="event-delete">Delete</a>', 'hoo-location-event-edit', 'delete', $event->id )
     );
 
     return sprintf( '%1$s %2$s', $event->label, $this->row_actions( $actions ) );
