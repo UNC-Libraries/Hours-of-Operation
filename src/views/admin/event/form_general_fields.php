7d7d01de217c398ba411f00b078dc718ca9a8f31
Steve Erhart <erhart@email.unc.edu>
2014-10-07 12:41:48 -0400

    changed label to title and added the category color as a data attribute

--- src/views/admin/event/form_general_fields.php
+++ src/views/admin/event/form_general_fields.php
@@ -2,12 +2,12 @@
   <li>
     <div class="js-wpt-field wpt-field js-wpt-textfield wpt-textfield">
       <div class="form-item form-item-textfield">
-        <label for="event_label" class="wpt-form-label wpt-form-textfield-label">Title / Label</label>
+        <label for="event_title" class="wpt-form-label wpt-form-textfield-label">Title / Label</label>
         <input type="text"
-               name="event[label]"
-               id="event_name"
+               name="event[title]"
+               id="event_title"
                class="wpt-form-textfield form-textfield textfield"
-               value="<?php echo $this['event']->label ?>"/>
+               value="<?php echo $this['event']->title ?>"/>
       </div>
     </div>
   </li>
@@ -15,10 +15,12 @@
     <div class="category">
       <div class="form-item form-item-select">
         <label for="event_category" class="wpt-form-label wpt-form-select-label">Category</label>
-        <select name="event[category]">
+        <select id="event_category" name="event[category]">
           <?php foreach( $this['event-categories'] as $category ) : ?>
             <option value="">Select a Category</option>
-            <option value="<?php echo $category->id ?>" <?php echo $this['event']->category == $category ? 'selected' : '' ?>>
+            <option value="<?php echo $category->id ?>"
+                    data-color="<?php echo $category->color ?>"
+              <?php echo $this['event']->category == $category ? 'selected' : '' ?>>
               <?php echo $category->name ?>
             </option>
           <?php endforeach ?>
