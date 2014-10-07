d1fcbf198520a4f23391fd8e62abc807c794e5d2
Steve Erhart <erhart@email.unc.edu>
2014-10-07 12:41:09 -0400

    changed input type to datetime and changed datetime format

--- src/views/admin/event/form_details_fields.php
+++ src/views/admin/event/form_details_fields.php
@@ -3,11 +3,11 @@
     <div class="js-wpt-field wpt-field js-wpt-textfield wpt-textfield">
       <div class="form-item form-item-textfield">
         <label for="event_start" class="wpt-form-label wpt-form-textfield-label">Start</label>
-        <input type="text"
+        <input type="datetime"
                name="event[start]"
                id="event_start"
                class="wpt-form-textfield form-textfield textfield datetimefield"
-               value="<?php echo is_object( $this['event']->start ) ? $this['event']->start->format( 'Y-m-d H:i' ) : '' ?>"/>
+               value="<?php echo is_object( $this['event']->start ) ? $this['event']->start->format( 'Y/m/d H:i' ) : '' ?>"/>
       </div>
     </div>
   </li>
@@ -15,11 +15,11 @@
     <div class="js-wpt-field wpt-field js-wpt-textfield wpt-textfield">
       <div class="form-item form-item-textfield">
         <label for="event_end" class="wpt-form-label wpt-form-textfield-label">End</label>
-        <input type="text"
+        <input type="datetime"
                name="event[end]"
                id="event_end"
                class="wpt-form-textfield form-textfield textfield datetimefield"
-               value="<?php echo is_object( $this['event']->end ) ? $this['event']->end->format( 'Y-m-d H:i' ) : '' ?>"/>
+               value="<?php echo is_object( $this['event']->end ) ? $this['event']->end->format( 'Y/m/d H:i' ) : '' ?>"/>
       </div>
     </div>
   </li>
