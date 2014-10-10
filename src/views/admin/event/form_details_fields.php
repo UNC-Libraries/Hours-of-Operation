ace410b5f4493c9b901f42ae3f57d6ee19690ee0
Steve Erhart <erhart@email.unc.edu>
2014-10-10 11:54:44 -0400

    set current timezone

--- src/views/admin/event/form_details_fields.php
+++ src/views/admin/event/form_details_fields.php
@@ -3,11 +3,12 @@
     <div class="js-wpt-field wpt-field js-wpt-textfield wpt-textfield">
       <div class="form-item form-item-textfield">
         <label for="event_start" class="wpt-form-label wpt-form-textfield-label">Start</label>
+        <?php $this['event']->start->setTimezone( new \DateTimeZone( get_option( 'timezone_string' ) ) ) ?>
         <input type="datetime"
                name="event[start]"
                id="event_start"
                class="wpt-form-textfield form-textfield textfield datetimefield"
-               value="<?php echo is_object( $this['event']->start ) ? $this['event']->start->format( 'Y/m/d H:i' ) : '' ?>"/>
+               value="<?php echo $this['event']->start->format( 'Y-m-d H:i' ) ?>"/>
       </div>
     </div>
   </li>
@@ -15,11 +16,12 @@
     <div class="js-wpt-field wpt-field js-wpt-textfield wpt-textfield">
       <div class="form-item form-item-textfield">
         <label for="event_end" class="wpt-form-label wpt-form-textfield-label">End</label>
+        <?php $this['event']->end->setTimezone( new \DateTimeZone( get_option( 'timezone_string' ) ) ) ?>
         <input type="datetime"
                name="event[end]"
                id="event_end"
                class="wpt-form-textfield form-textfield textfield datetimefield"
-               value="<?php echo is_object( $this['event']->end ) ? $this['event']->end->format( 'Y/m/d H:i' ) : '' ?>"/>
+               value="<?php echo $this['event']->end->format( 'Y-m-d H:i' )?>">
       </div>
     </div>
   </li>
