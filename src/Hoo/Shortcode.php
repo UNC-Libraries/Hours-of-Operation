1dae50b44bfd4842feae14ca579e1f531df062b6
Steve Erhart <erhart@email.unc.edu>
2014-10-23 13:34:17 -0400

    dirty hack for listing sublocations

--- src/Hoo/Shortcode.php
+++ src/Hoo/Shortcode.php
@@ -33,7 +33,22 @@ class Shortcode {
 
   public function full() {
     $locations_repo = $this->entity_manager->getRepository( '\Hoo\Model\Location' );
-    $locations = $locations_repo->findBy( array(), array( 'position' => 'asc' ) );
+    $locations = $locations_repo->findBy( array( 'parent' => null ), array( 'position' => 'asc' ) );
+    /* quick hack to put the sublocations under the parent
+       TODO: rewrite this in the model 
+    */
+    $locations = array_reduce(
+      $locations,
+      function( $locations, $location ) {
+        $locations[] = $location;
+        foreach( $location->sublocations->toArray() as $sub ) {
+          $locations[] = $sub;
+        }
+        return $locations;
+      },
+      array() );
+
+
     $view = new View( 'shortcode/location' );
     $view->render( array( 'locations' => $locations,
                           'now' => new \DateTime( null, new \DateTimeZone( get_option( 'timezone_string' ) ) )) );
