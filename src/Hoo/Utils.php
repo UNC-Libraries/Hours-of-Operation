99dc77666f281cf97c78c73fd1eaa7cf0720d683
Steve Erhart <erhart@email.unc.edu>
2014-10-27 11:06:57 -0400

    added function to format a time range

--- src/Hoo/Utils.php
+++ src/Hoo/Utils.php
@@ -13,6 +13,13 @@ class Utils {
 
     return array_key_exists( $role, (array) $user->allcaps );
   }
+  
+  static public function format_as_time_range( $start, $end ) {
+    $start_format = ( $start->format('i') == '00' ? 'g ' : 'g:i ' ) . 'a';
+    $end_format = ( $end->format('i') == '00' ? 'g ' : 'g:i ' ) . 'a';
+    
+    return sprintf( '%s - %s', $start->format( $start_format), $end->format( $end_format ) );
+  }
 }
 
 ?>
