77cf49deaaa6f1e474982b974424896de0f232c6
Steve Erhart <erhart@email.unc.edu>
2014-11-17 15:31:14 -0500

    only show main widget if we have locations that we can actually show :D

--- src/views/shortcode/location.php
+++ src/views/shortcode/location.php
@@ -1,13 +1,17 @@
 <div id="hoo-main">
-  <div id="location-list-container">
-    <?php $this->include_file( 'shortcode/location_list' ) ?>
-  </div>
-
-  <div id="panel-container">
-    <?php $this->include_file( 'shortcode/location_detail' ) ?>
-
-    <div id="hoo-map">
-      <div id="map-canvas"></div>
+    <?php if ( count( $this['locations'] ) > 0 ) : ?>
+    <div id="location-list-container">
+        <?php $this->include_file( 'shortcode/location_list' ) ?>
     </div>
-  </div>
+
+    <div id="panel-container">
+        <?php $this->include_file( 'shortcode/location_detail' ) ?>
+
+        <div id="hoo-map">
+            <div id="map-canvas"></div>
+        </div>
+    </div>
+    <?php else : ?>
+    <p>No complete locations to display.</p>
+    <?php endif ?>
 </div>
