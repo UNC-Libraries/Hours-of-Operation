0c5546731f83b41342056efc21d13d24d778b97e
Steve Erhart <erhart@email.unc.edu>
2014-11-25 13:55:20 -0500

    added api shortcode

--- src/Hoo/Shortcode.php
+++ src/Hoo/Shortcode.php
@@ -8,32 +8,52 @@ class Shortcode {
 
         add_shortcode( 'hoo', array( $this, 'hoo' ) );
 
-        add_action( 'wp_enqueue_scripts', array( $this, 'init' ) );
+        /*
+           HACK:
+           Registers the hoo-api shortcode. However, the template_redirect callback should intercept
+           and check for the shortcode before wp can render anything
+           hoo_api will send some json and exit
+           TODO: find a better way to do the api? :D
+         */
+        add_shortcode( 'hoo-api', array( $this, 'hoo_api' ) );
+        add_action( 'template_redirect', array( $this, 'hoo_api' ) );
+    }
+
+    public function enqueue_script( $widget ) {
+        switch( $widget ){
+            case 'full':
+                wp_enqueue_style( 'shortcode-main' );
+                wp_enqueue_script( 'shortcode-main' );
+                break;
+            default:
+        }
+    }
+    public function hoo_api() {
+        global $post;
+
+        // /if the page containts the hoo-api shortcode send json and exit :}
+        if ( is_a( $post, 'WP_Post' ) && has_shortcode( $post->post_content, 'hoo-api' ) ) {
+            $locations_repo = $this->entity_manager->getRepository( '\Hoo\Model\Location' );
+
+            if ( isset( $_GET['location_id'] ) ) {
+                $location = $locations_repo->findBy( array( 'id' => $_GET['location_id'], 'is_visible' => true ) )[0];
+                $hours = $location->get_hours_for_date( $_GET['date'] );
+                wp_send_json( $hours );
+            }
+            exit;
+        }
     }
 
     public function hoo( $attributes ) {
-        $attributes = shortcode_atts( array( 'widget' => 'full' ), $attribtues, 'hoo' );
+        $attributes = shortcode_atts( array( 'widget' => 'full' ), $attributes, 'hoo' );
 
-        if( method_exists( $this, $attributes['widget'] ) ) {
+
+        if ( method_exists( $this, $attributes['widget'] ) && 'hoo_api' != $attributes['widget'] ) {
+            $this->enqueue_script( $attributes['widget'] );
             $this->$attributes['widget']();
         } else {
             return 'bad widget attribute!!';
         }
-    }
-
-    public function init() {
-        global $post;
-
-        // check the current page for the hoo shortcode
-        if ( is_a( $post, 'WP_Post' ) && has_shortcode( $post->post_content, 'hoo' ) ) {
-
-            wp_enqueue_style( 'shortcode-main' );
-            wp_enqueue_script( 'shortcode-main' );
-        }
-    }
-
-    public function api() {
-
     }
 
     public function full() {
