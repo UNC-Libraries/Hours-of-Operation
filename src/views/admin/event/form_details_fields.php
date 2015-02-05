897b409dc4080129447e644e783db269b4656320
Steve Segedy <segedy@email.unc.edu>
2015-02-05 16:29:18 -0500

    fixing display of custom weekly, monthly, and yearly fields in hours events

--- src/views/admin/event/form_details_fields.php
+++ src/views/admin/event/form_details_fields.php
@@ -111,41 +111,41 @@
             <div class="rrule-custom weekly <?php if ( ! ( $this['event']->is_custom_rrule && $this['event']->recurrence_rule->getFreqAsText() == 'WEEKLY' ) ) echo 'is-hidden' ?>">
                 <?php foreach ( array( 'SU' => 'Sunday', 'MO' => 'Monday', 'TU' => 'Tuesday', 'WE' => 'Wednesday', 'TH' => 'Thursday', 'FR' => 'Friday', 'SA' => 'Saturday' ) as $abbrv => $full) : ?>
                     <label for="<?php echo sprintf( 'event_recurrence_rule_custom_byday_%s', $abbrv ) ?>" class="wpt-form-label wpt-form-checkbox-label">
-                        <?php echo $full ?>
-                    </label>
-                    <input type="checkbox"
+                        <input type="checkbox"
                            id="<?php sprintf( 'event_recurrence_rule_custom_byday_%s', $abbrv ) ?>"
                            class="hoo-rrule"
                            name="event_recurrence_rule_custom[BYDAY][]"
                            value="<?php echo $abbrv ?>"
                            <?php if ( $this['event']->is_custom_rrule && $this['event']->recurrence_rule->getByDay() && in_array( $abbrv, $this['event']->recurrence_rule->getByDay() ) ) echo 'checked' ?>/>
+                        <?php echo $full ?>
+                    </label>
                 <?php endforeach ?>
             </div>
 
             <div class="rrule-custom monthly <?php if ( ! ( $this['event']->is_custom_rrule && $this['event']->recurrence_rule->getByMonthDay() == 'MONTHLY' ) ) echo 'is-hidden' ?>">
                 <?php foreach( range( 1, 31 ) as $day ) : ?>
                     <label for="<?php sprintf( 'event_recurrence_rule_custom_bymonth_%s', $day ) ?>">
-                        <?php echo $day ?>
-                    </label>
-                    <input type="radio"
+                        <input type="radio"
                            id="<?php sprintf( 'event_recurrence_rule_custom_by_month_%s', $day ) ?>"
                            class="hoo-rrule"
                            name="event_recurrence_rule_custom[BYMONTH][]"
                            value="<?php echo $day ?>"
                            <?php echo $this['event']->is_custom_rrule && $this['event']->recurrence_rule->getByMonthDay() == $day ? 'checked' : '' ?>/>
+                        <?php echo $day ?>
+                    </label>
                 <?php endforeach ?>
             </div>
 
             <div class="rrule-custom yearly <?php if ( ! ( $this['event']->is_custom_rrule && $this['event']->recurrence_rule->getFreqAsText() == 'YEARLY' ) ) echo 'is-hidden' ?>">
                 <?php foreach( range( 1, 12 ) as $month ): ?>
                     <label for="<?php sprintf( 'event_recurrence_rule_custom_byyear_%s', $month ) ?>">
-                        <?php echo \DateTime::createFromFormat( '!m', $month )->format( 'M' ) ?>
-                    </label>
-                    <input type="radio"
+                        <input type="radio"
                            id="<?php sprintf( 'event_recurrence_rule_custom_byyear_%s', $month ) ?>"
                            class="hoo-rrule"
                            name="event_recurrence_rule_custom[BYYEAR][]"
                            value="<?php echo $month ?>"/>
+                        <?php echo \DateTime::createFromFormat( '!m', $month )->format( 'M' ) ?>
+                    </label>
                 <?php endforeach ?>
             </div>
         </div>
