c6ab67256a0c0f5879e7ff3639cc91bc3f20e9a2
Steve Segedy <segedy@email.unc.edu>
2015-02-06 10:52:30 -0500

    modifying header to fix display of breadcrumbs

--- src/views/admin/partials/header.php
+++ src/views/admin/partials/header.php
@@ -1,25 +1,29 @@
 <div class="hoo-head">
-    <span>HoO</span>
-    <a href="?page=<?php echo isset( $this['hoobert-link'] ) ? $this['hoobert-link'] : 'hoo' ?>">
-        <img src="<?php echo HOO__PLUGIN_URL . 'assets/images/hoo-100.png' ?>"/>
-    </a>
-    <h2 id="wphead">
-        <?php echo $this['title'] ?>
-        <?php if ( isset( $this['add-new-page'] ) ) : ?>
-            <a class="add-new-h2" href="?page=<?php echo $this['add-new-page'] ?>">Add New</a>
+    <div class="hoo-logo">
+        <span>HoO</span>
+        <a href="?page=<?php echo isset( $this['hoobert-link'] ) ? $this['hoobert-link'] : 'hoo' ?>">
+            <img src="<?php echo HOO__PLUGIN_URL . 'assets/images/hoo-100.png' ?>"/>
+        </a>
+    </div>
+    <div class="hoo-title">
+        <?php if ( isset( $this['breadcrumbs'] ) ) : ?>
+        <ol class="breadcrumbs">
+            <?php $last_crumb = end( $this['breadcrumbs'] ); reset( $this['breadcrumbs'] ); ?>
+            <?php foreach( $this['breadcrumbs'] as $title => $url ) : ?>
+                <?php  if ( $last_crumb == $url ) : ?>
+                    <li class="active"><?php echo $title ?></li>
+                <?php else : ?>
+                    <li><a href="admin.php?page=<?php echo $url ?>"><?php echo $title ?></a> | </li>
+                <?php endif ?>
+            <?php endforeach ?>
+        </ol>
         <?php endif ?>
-    </h2>
-    <?php if ( isset( $this['breadcrumbs'] ) ) : ?>
-    <ol class="breadcrumbs">
-        <?php $last_crumb = end( $this['breadcrumbs'] ); reset( $this['breadcrumbs'] ); ?>
-        <?php foreach( $this['breadcrumbs'] as $title => $url ) : ?>
-            <?php  if ( $last_crumb == $url ) : ?>
-                <li class="active"><?php echo $title ?></li>
-            <?php else : ?>
-                <li><a href="admin.php?page=<?php echo $url ?>"><?php echo $title ?></a></li>
+        <h2 id="wphead">
+            <?php echo $this['title'] ?>
+            <?php if ( isset( $this['add-new-page'] ) ) : ?>
+                <a class="add-new-h2" href="?page=<?php echo $this['add-new-page'] ?>">Add New</a>
             <?php endif ?>
-        <?php endforeach ?>
-    </ol>
-    <?php endif ?>
-    <?php $this->include_file( 'admin/partials/notifications' ) ?>
+        </h2>
+        <?php $this->include_file( 'admin/partials/notifications' ) ?>
+    </div>
 </div>
