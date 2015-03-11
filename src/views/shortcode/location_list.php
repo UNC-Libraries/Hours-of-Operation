a6faf3e07dc606c8f107e7a6608c2b061a580ba5
Steve Erhart <erhart@email.unc.edu>
2015-03-11 13:47:19 -0400

    changed location list into a table and some styled it up!

--- src/views/shortcode/location_list.php
+++ src/views/shortcode/location_list.php
@@ -1,13 +1,11 @@
-<ul id="locations-list">
-  <li class="list-header">
-    <div class="location-name">
-      <span>Location Name</span>
-    </div>
-    <div class="location-status">
-      <span>
-        at <?php echo $this['now']->format( 'h:i a') ?>
-      </span>
-    </div>
-  </li>
-  <?php $this->include_file( 'shortcode/location_list_rows' ) ?>
-</ul>
+<table id="locations-list">
+    <thead>
+        <tr class="list-header">
+            <th class="location-name">Location Name</th>
+            <th class="location-status">at <?php echo $this['now']->format( 'h:i a') ?></th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php $this->include_file( 'shortcode/location_list_rows' ) ?>
+    </tbody>
+</table>
--- src/views/shortcode/location_list_rows.php
+++ src/views/shortcode/location_list_rows.php
@@ -1,22 +1,19 @@
 <?php foreach( $this['locations'] as $location ) : ?>
-  <?php $current_hours = $location->get_hours_for_date( null ); ?>
-  <li class="location-row" data-panel="panel-<?php echo $location->id ?>" data-lat="<?php echo $location->address->lat ?>" data-lon="<?php echo $location->address->lon ?>" data-id="<?php echo $location->id ?>">
-    <div class="location-name">
-      <span>
-        <a href="#location">
-          <?php echo $location->name ?>
-        </a>
-      </span>
-    </div>
-    <div class="location-status">
-      <?php if ( is_null( $current_hours ) ) : ?>
-        <p>N/A</p>
-      <?php elseif ( \Hoo\Utils::is_open( $current_hours ) ) : ?>
-        <p>Open</p>
-        <p>Until <?php echo \Hoo\Utils::format_time( $current_hours['close'] )?> </p>
-      <?php else : ?>
-        <p>Closed</p>
-      <?php endif ?>
-    </div>
-  </li>
+    <?php $current_hours = $location->get_hours_for_date( null ); ?>
+    <tr class="location-row" data-panel="panel-<?php echo $location->id ?>" data-lat="<?php echo $location->address->lat ?>" data-lon="<?php echo $location->address->lon ?>" data-id="<?php echo $location->id ?>">
+        <td class="location-name">
+            <a href="#location">
+                <?php echo $location->name ?>
+            </a>
+        </td>
+        <td class="location-status">
+            <?php if ( is_null( $current_hours ) ) : ?>
+                <span class="na">N/A</span>
+            <?php elseif ( \Hoo\Utils::is_open( $current_hours ) ) : ?>
+                <span class="until">Until <?php echo \Hoo\Utils::format_time( $current_hours['close'] )?> </span>
+            <?php else : ?>
+                <span class="closed">Closed</span>
+            <?php endif ?>
+        </td>
+    </tr>
 <?php endforeach ?>
