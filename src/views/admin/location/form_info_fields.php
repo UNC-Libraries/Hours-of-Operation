d816b1edbea395080f01b239c2500d34f542a2de
Steve Erhart <erhart@email.unc.edu>
2015-03-16 11:11:31 -0400

    added missing phone number field.  fixes APPDEV-4102

--- src/views/admin/location/form_info_fields.php
+++ src/views/admin/location/form_info_fields.php
@@ -12,6 +12,18 @@
         </div>
     </li>
     <li>
+        <div class="js-wpt-field wpt-field js-wpt-textfield wpt-textfield">
+            <div class="form-item form-item-textfield">
+                <label for="location_phone" class="wpt-form-label wpt-form-textfield-label">Phone Number</label>
+                <input type="url"
+                       name="location[phone]"
+                       id="location_phone"
+                       class="wpt-form-textfield form-textfield textfield"
+                       value="<?php echo $this['location']->phone ?>"/>
+            </div>
+        </div>
+    </li>
+    <li>
         <div class="parent">
             <div class="form-item form-item-select">
                 <label for="location_parent" class="wpt-form-label wpt-form-select-label">Parent Location</label>
