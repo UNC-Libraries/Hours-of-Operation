f2e911dd9a11a32416798c831ff795a6fef81924
Steve Erhart <erhart@email.unc.edu>
2015-03-25 12:12:46 -0400

    added wpt-form-label class for formatting

--- src/views/admin/location/form_address_fields.php
+++ src/views/admin/location/form_address_fields.php
@@ -2,7 +2,7 @@
     <li>
         <div class="js-wpt-field wpt-field js-wpt-textfield wpt-textfield">
             <div class="form-item form-item-textfield">
-                <label for="address_line1">Line 1</label>
+                <label class="wpt-form-label" for="address_line1">Line 1</label>
                 <input type="text" name="location[address][line1]" id="address_line1" size="60" maxlength="256 "value="<?php echo $this['location']->address->line1 ?>"/>
             </div>
         </div>
@@ -10,7 +10,7 @@
     <li>
         <div class="js-wpt-field wpt-field js-wpt-textfield wpt-textfield">
             <div class="form-item form-item-textfield">
-                <label for="address_line2">Line 2</label>
+                <label class="wpt-form-label" for="address_line2">Line 2</label>
                 <input type="text" name="location[address][line2]" id="address_line2" size="60" maxlangth="256" value="<?php echo $this['location']->address->line2 ?>"/>
             </div>
         </div>
@@ -18,7 +18,7 @@
     <li>
         <div class="js-wpt-field wpt-field js-wpt-textfield wpt-textfield">
             <div class="form-item form-item-textfield">
-                <label for="address_line2">Line 3</label>
+                <label class="wpt-form-label" for="address_line2">Line 3</label>
                 <input type="text" name="location[address][line3]" id="address_line3" size="60" maxlangth="256" value="<?php echo $this['location']->address->line2 ?>"/>
             </div>
         </div>
@@ -26,7 +26,7 @@
     <li>
         <div class="js-wpt-field wpt-field js-wpt-textfield wpt-textfield">
             <div class="form-item form-item-textfield">
-                <label for="address_line3">City</label>
+                <label class="wpt-form-label" for="address_line3">City</label>
                 <input type="text" name="location[address][city]" id="address_city" size="60" maxlangth="256" value="<?php echo $this['location']->address->city ?>"/>
             </div>
         </div>
@@ -34,7 +34,7 @@
     <li>
         <div class="js-wpt-field wpt-field js-wpt-textfield wpt-textfield">
             <div class="form-item form-item-textfield">
-                <label for="address_line3">State</label>
+                <label class="wpt-form-label" for="address_line3">State</label>
                 <input type="text" name="location[address][state]" id="address_state" size="2" maxlength="256" value="<?php echo $this['location']->address->state ?>"/>
             </div>
         </div>
@@ -42,7 +42,7 @@
     <li>
         <div class="js-wpt-field wpt-field js-wpt-textfield wpt-textfield">
             <div class="form-item form-item-textfield">
-                <label for="address_line3">Zip</label>
+                <label class="wpt-form-label" for="address_line3">Zip</label>
                 <input type="text" name="location[address][zip]" id="address_zip" size="20" maxlength="256" value="<?php echo $this['location']->address->zip ?>"/>
             </div>
         </div>
@@ -50,7 +50,7 @@
     <li>
         <div class="js-wpt-field wpt-field js-wpt-textfield wpt-textfield">
             <div class="form-item form-item-textfield">
-                <label for="address_lat">Latitude</label>
+                <label class="wpt-form-label" for="address_lat">Latitude</label>
                 <input type="text"
                        name="location[address][lat]"
                        id="address_lat"
@@ -63,7 +63,7 @@
     <li>
         <div class="js-wpt-field wpt-field js-wpt-textfield wpt-textfield">
             <div class="form-item form-item-textfield">
-                <label for="address_lon">Longitude</label>
+                <label class="wpt-form-label" for="address_lon">Longitude</label>
                 <input type="text" name="location[address][lon]" id="address_lon" size="20" maxlength="256" value="<?php echo $this['location']->address->lon ?>"/>
             </div>
         </div>
