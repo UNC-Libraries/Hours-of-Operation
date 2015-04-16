3ab1ee535b033ae48a5825a87ef388fcacd8d57c
Steve Erhart <erhart@email.unc.edu>
2015-04-16 15:19:35 -0400

    highlight active location in location list.  fixes APPDEV-4265

--- assets/js/shortcode-main.js
+++ assets/js/shortcode-main.js
@@ -5,54 +5,54 @@ jQuery( function( $ ) {
         $hours_calendars = $( '.hours-calendar' ),
         $locations_list = $( '#locations-list'),
         $panel_container = $( '#panel-container' );
-        $hoo_main       = $( '#hoo-main'),
-        $hoo_map        = $( '#hoo-map'),
+    $hoo_main       = $( '#hoo-main'),
+    $hoo_map        = $( '#hoo-map'),
 
-        map_options = {
-            center: {
-                lat: 0,
-                lng: 0
-            },
-            zoom: 8,
-            disableDefaultUI: true
+    map_options = {
+        center: {
+            lat: 0,
+            lng: 0
         },
+        zoom: 8,
+        disableDefaultUI: true
+    },
 
 
-        locations_map = new google.maps.Map( document.getElementById( 'map-canvas' ), map_options ),
-        locations_bounds = new google.maps.LatLngBounds(),
-        location_markers = {},
+    locations_map = new google.maps.Map( document.getElementById( 'map-canvas' ), map_options ),
+    locations_bounds = new google.maps.LatLngBounds(),
+    location_markers = {},
 
-        create_location_marker = function( location ) {
-            var lat = location.dataset.lat,
-                lon = location.dataset.lon,
-                lat_lon = new google.maps.LatLng( lat, lon ),
+    create_location_marker = function( location ) {
+        var lat = location.dataset.lat,
+            lon = location.dataset.lon,
+            lat_lon = new google.maps.LatLng( lat, lon ),
 
-                marker = new google.maps.Marker( {
-                    map: locations_map,
-                    draggable: false,
-                    animation: google.maps.Animation.DROP,
-                    position: lat_lon } ),
+            marker = new google.maps.Marker( {
+                map: locations_map,
+                draggable: false,
+                animation: google.maps.Animation.DROP,
+                position: lat_lon } ),
 
-                slideout_panel = function() {
-                    $( '#' + location.dataset.panel ).show( 'slide', {direction: 'left', easing: 'easeOutBounce' } , 800);
-                    $('.hours-calendar').fullCalendar( 'render' );
-                },
-                highlight_row = function() { $( '.location-row[data-id="' + location.dataset.id + '"]').addClass( 'highlight' ); },
-                remove_highlight = function() {$( '.location-row[data-id="' + location.dataset.id + '"]').removeClass( 'highlight' ); };
+            slideout_panel = function() {
+                $( '#' + location.dataset.panel ).show( 'slide', {direction: 'left', easing: 'easeOutBounce' } , 800);
+                $('.hours-calendar').fullCalendar( 'render' );
+            },
+            highlight_row = function() { $( '.location-row[data-id="' + location.dataset.id + '"]').addClass( 'highlight' ); },
+            remove_highlight = function() {$( '.location-row[data-id="' + location.dataset.id + '"]').removeClass( 'highlight' ); };
 
-            if ( lat.length && lon.length ) {
-                locations_bounds.extend( lat_lon );
-            }
-            google.maps.event.addListener( marker, 'mouseover', highlight_row );
-            google.maps.event.addListener( marker, 'mouseout', remove_highlight );
-            google.maps.event.addListener( marker, 'click', slideout_panel );
+        if ( lat.length && lon.length ) {
+            locations_bounds.extend( lat_lon );
+        }
+        google.maps.event.addListener( marker, 'mouseover', highlight_row );
+        google.maps.event.addListener( marker, 'mouseout', remove_highlight );
+        google.maps.event.addListener( marker, 'click', slideout_panel );
 
-            return marker;
-        };
+        return marker;
+    };
 
     $hoo_main.height( $locations_list.height() );
     $hoo_map.height( $panel_container.height() );
-    
+
 
 
     $hoo_list_rows.each( function( index, location ) {
@@ -73,8 +73,8 @@ jQuery( function( $ ) {
                 center: 'title',
                 right: 'today prev,next'
             },
-               
-            
+
+
             events: function( cal_start, cal_end, tz, cb ) {
 
                 $.ajax( {
@@ -105,22 +105,30 @@ jQuery( function( $ ) {
     $hoo_list_rows.on( 'click', function( e ) {
         var location_id = $( this ).data( 'panel' ).split( '-' )[1],
             $this_panel = $( '#' + $( this ).data( 'panel' ) ),
-            $visible_panel = $('.panel:visible');
+            $visible_panel = $('.panel:visible'),
 
-        if ( $this_panel.is( ':visible' ) ) {
+            $prev_active = $( '.location-row.active' );
+
+        if ( $prev_active[0] === $( this )[0] ) {
+            console.log('sup');
             $this_panel.hide( 'slide', { direction: 'left', easing: 'easeOutExpo' }, 500 );
+            $prev_active.removeClass( 'active' );
         }
-        else if ( $visible_panel.length ) {
+        else if ( $prev_active.length ) {
+            console.log($prev_active);
             $this_panel.show('fade', {
                 complete: function(){
                     $visible_panel.hide('fade');
                     $('.hours-calendar').fullCalendar( 'render' );
                 }
             });
+            $prev_active.removeClass( 'active' );
+            $( this ).addClass( 'active' );
         } else {
             $this_panel.show( 'slide', {direction: 'left', easing: 'easeOutBounce' } , 800);
             // render as soon as possible.  hopefully this will always render and we don't have to wait for the animation to complete
             $('.hours-calendar').fullCalendar( 'render' );
+            $( this ).addClass( 'active' );
         }
 
     });
